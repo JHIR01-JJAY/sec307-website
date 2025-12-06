d0c3556b8f3f1758ed86c92708b2b071d5c248f6
JHIR01 <henryirivere@gmail.com>
2025-12-06 03:34:49 -0500

    this is a clone of a good looking website

--- src/App.jsx
+++
@@ -1,1 +0,0 @@
-import React from 'react';import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';import Home from './pages/Home.jsx';export default function App(){return(<BrowserRouter><Link to='/'>Home</Link><Routes><Route path='/' element={<Home/>}/></Routes></BrowserRouter>);}
